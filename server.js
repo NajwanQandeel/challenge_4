6b3219fd0aa598123993e47fe3fa98d9ad9b531c
NajwanQandeel <najwan.qandeel@gmail.com>
2020-11-05 12:30:21 +0200

    By works, I meant 'doesnt work'. Works now..

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
 
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/client/dist/index.html");
+  });
+
+  app.post('/upload_json', (req, res) => {
+     console.log(req.body)
+     
+
+    res.end(result)
+});
+
+
+
+
+
+  app.listen(3000,()=>{
+    console.log("Server running on port 3000");
+   });
